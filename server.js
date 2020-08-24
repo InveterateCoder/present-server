e52d806957f5d4184cf4cacebc18472489282774
InveterateCoder <inveterate.coder@gmail.com>
2020-08-24 11:19:33 +0300

    dddd

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 //const cors = require('cors')
 const apiRouter = require('./server/apiRouter')
 const { connectToDb } = require('./server/db');
+console.log('aaaaaaaaaaaadkfjdkfjkdjalsfjalsfjlas kasdjfka jkdfsaj kfdjs')
 
 (async function () {
   await connectToDb()
